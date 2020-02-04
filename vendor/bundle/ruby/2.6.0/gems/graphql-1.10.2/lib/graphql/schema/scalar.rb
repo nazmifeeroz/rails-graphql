0c1559afd44fe5fd7dd1bc1a9042f35c5447731f
Nazmi <nazmifeeroz@hotmail.com>
2020-02-04 15:05:58 +0800

    :wrench: update bundle

---
+++ vendor/bundle/ruby/2.6.0/gems/graphql-1.10.2/lib/graphql/schema/scalar.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+module GraphQL
+  class Schema
+    class Scalar < GraphQL::Schema::Member
+      extend GraphQL::Schema::Member::AcceptsDefinition
+      extend GraphQL::Schema::Member::ValidatesInput
+
+      class << self
+        def coerce_input(val, ctx)
+          val
+        end
+
+        def coerce_result(val, ctx)
+          val
+        end
+
+        def to_graphql
+          type_defn = GraphQL::ScalarType.new
+          type_defn.name = graphql_name
+          type_defn.description = description
+          type_defn.coerce_result = method(:coerce_result)
+          type_defn.coerce_input = method(:coerce_input)
+          type_defn.metadata[:type_class] = self
+          type_defn.default_scalar = default_scalar
+          type_defn.ast_node = ast_node
+          type_defn
+        end
+
+        def kind
+          GraphQL::TypeKinds::SCALAR
+        end
+
+        def default_scalar(is_default = nil)
+          if !is_default.nil?
+            @default_scalar = is_default
+          end
+          @default_scalar
+        end
+
+        def default_scalar?
+          @default_scalar ||= false
+        end
+
+        def validate_non_null_input(value, ctx)
+          result = Query::InputValidationResult.new
+          if coerce_input(value, ctx).nil?
+            str_value = if value == Float::INFINITY
+              ""
+            else
+              " #{GraphQL::Language.serialize(value)}"
+            end
+            result.add_problem("Could not coerce value#{str_value} to #{graphql_name}")
+          end
+          result
+        end
+      end
+    end
+  end
+end
